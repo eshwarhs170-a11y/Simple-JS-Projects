b83458b3e32e2e5f46ca31b5ac3a05b64d0bd4eb
Eshwar H S <eshwarhs170@gmail.com>
2026-04-09 21:47:37 +0530

    Initial commit: Counter app with logic and box styling

---
+++ script.js
@@ -0,0 +1,22 @@
+console.log('hello');
+// document.querySelector('button')
+// document.getElementById(zero)
+let count=0;
+let a=document.querySelector("#zero")
+// a.addEventListener("click",function(){
+// a.innerHTML=count++;
+// })
+console.log(a)
+let b=document.querySelector(".Increase") 
+b.addEventListener("click",function(){
+    a.innerHTML=++count
+})
+let c=document.querySelector(".Decrease") 
+c.addEventListener("click",function(){
+    if(count>0){
+        a.innerHTML= count--;
+    }
+    else{
+        a.innerHTML=count;
+    }
+})
